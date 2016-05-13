5916f88217b787b167a64fb30311b944cdffa6f5
Katherine Newling <kn675@biolmac432.york.ac.uk>
2016-05-13 10:58:58 +0100

    importBLASTselfmatch.py: updated to not insert each internal hit in a collection in mongoDB.

--- tools/importBLASTselfmatch.py
+++ tools/importBLASTselfmatch.py
@@ -1,5 +1,38 @@
+from pymongo import MongoClient
 
-f = open('selfmatches.txt')
+import argparse
+
+parser = argparse.ArgumentParser(description="Import a text file containing BLAST database internal hits into a MongoDB collection.")
+
+parser.add_argument("filename", 
+                    nargs=1, 
+                    type=str, 
+                    help="Name of the file to import")
+parser.add_argument("--host", 
+                    type=str,
+                    default='localhost',
+                    help="Hostname for the MongoDB database (default=localhost)")
+parser.add_argument("--port", 
+                    type=str,
+                    default=27017,
+                    help="Port where MongoDB is listening (default=27017)")
+parser.add_argument("--database", 
+                    type=str,
+                    default='symbiont',
+                    help="Name of the database to store the data in (default=symbiont)")
+parser.add_argument("--collection", 
+                    type=str,
+                    default='internal_blast_hits',
+                    help="Name of the collection to use to store the BLAST hits (default=internal_blast_hits)") 
+
+args = parser.parse_args()
+
+client = MongoClient(args.host, args.port)
+
+db = client[args.database]
+internal_blast_hits = db[args.collection]
+
+f = open(args.filename[0])
 
 qidDict = {} #Dictionary to match qids to qgenomes
 matches = [] #list of all results
@@ -26,8 +59,9 @@ for match in matches:
 	sid = match["sid"]
 	sgenome = qidDict[sid]
 	match["sgenome"] = sgenome
+	
+	#insert the BLAST match into the collection
+	internal_blast_hits.insert_one(match)
 
-for i in range(0,100):
-	if(matches[i]["qid"] == matches[i]["sid"]):
-		print("uh oh")
+
 
