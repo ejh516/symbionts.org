b97370ab74a06a2f7c7e7a38a76e780e0765c2d0
Katherine Newling <kn675@biolmac432.york.ac.uk>
2016-05-16 15:52:28 +0100

    createOrthologuesCollection.py: added an index for qid.

--- tools/createOrthologuesCollection.py
+++ tools/createOrthologuesCollection.py
@@ -37,15 +37,17 @@ internal_blast_hits_indices = internal_blast_hits.index_information().keys()
 
 if 'evalue_score' not in internal_blast_hits_indices:
     internal_blast_hits.create_index([("evalue",1), ("score", -1)], name='evalue_score')
-
+if 'qid' not in internal_blast_hits_indices:
+    internal_blast_hits.create_index([("qid",1)], name='qid')
 
 # get all distinct qids in the collection
 
 qids = internal_blast_hits.distinct("qid")
+print len(qids)
 
 # for every qid in the collection, find the top hit (based on evalue, then score) for each genome for which there is a hit
 
-for qid in set(qids[:20]):
+for qid in set(qids):
 
      # find distinct sgenomes for that qid
      sgenomes = internal_blast_hits.distinct("sgenome", {'qid':qid})
@@ -53,7 +55,7 @@ for qid in set(qids[:20]):
      for sgenome in sgenomes:
 
           #check that this qid doesn't already have a BBH in this genome
-          if (orthologues.find({"qid":qid, "sgenome":sgenome}).limit(1).count() == 0):               
+          if (orthologues.find({"sid":qid, "qgenome":sgenome}).limit(1).count() == 0):               
 
                # get best hit for that qid and that sgenome after sorting by evalue and then score
                q_hit = internal_blast_hits.find({"qid":qid, "sgenome":sgenome}).sort([["evalue",1], ["score",-1]])
@@ -62,12 +64,12 @@ for qid in set(qids[:20]):
                # see if the sid in best hit has the qid as best hit in qid's genome
                qgenome = internal_blast_hits.find_one({"qid":qid},{"qgenome":1})
                s_hit = internal_blast_hits.find({"qid":q_best_hit["sid"], "sgenome":qgenome["qgenome"]}).sort([["evalue",1], ["score",-1]])
-               s_best_hit = s_hit[0]
-
+               
                if (s_hit.count()>0):
+                    s_best_hit = s_hit[0]
                     if (s_best_hit["sid"] == qid):
-                         # print "Found a BBH: %s, %s and %s, %s" %(q_hit[0]["qid"],q_hit[0]["qgenome"],q_hit[0]["sid"],q_hit[0]["sgenome"])
                           orthologues.insert_one({"qid": q_best_hit["qid"], "sid": q_best_hit["sid"], "qgenome": q_best_hit["qgenome"], "sgenome": q_best_hit["sgenome"]})
+
                          
 
 
