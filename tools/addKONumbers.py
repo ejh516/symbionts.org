8fc14e1c83dd68fe4dbaaf2ce4523ef077920dbe
Katherine Newling <kn675@biolmac432.york.ac.uk>
2016-05-18 14:17:08 +0100

    addKONumbers.py: added functionality for parsing KO feature file and creating dictionary to link KO numbers to name, pathway, etc.

--- tools/addKONumbers.py
+++ tools/addKONumbers.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
 
 import argparse
+import re
 
-parser = argparse.ArgumentParser(description="Add KO numbers, name, definition, EC number and pathway name to CDSs in features collection.")
+parser = argparse.ArgumentParser(description="Add KO number, name, definition, EC number and pathway name to CDSs in features collection.")
 
 parser.add_argument("KOFeatureFilename", 
                     nargs=1, 
@@ -37,5 +38,42 @@ features = db[args.featurecollection]
 KOFeatureFilename = args.KOFeatureFilename[0]
 KODetailsFilename = args.KODetailsFilename[0]
 
-KOFeatureFile = open(KOFeatureFilename, "r")
+KOfeatures = {}
+with open(KOFeatureFilename, "r") as KOFeatureFile:
+     for line in KOFeatureFile:
+          if line.startswith('A'):
+               parsedline = re.split('<b>|</b>', line)
+               pathA = parsedline[1]
+               
+          elif line.startswith('B'):
+               parsedline = re.split('<b>|</b>', line)
+               if len(parsedline) > 1:
+                    pathB = parsedline[1]
+              
+          elif line.startswith('C'):
+               parsedline = re.split('    ', line)[1]
+               pathNumber = parsedline[0:5]
+               pathC = parsedline[6:].split('[')[0][:-1]
+          
+          elif line.startswith('D'):
+               parsedline = re.split('  |;', line)
+               KONumber = parsedline[3]
+               KOfeatures[KONumber] = {}
+               KOfeatures[KONumber]["KO_name"] = parsedline[4]
+               rest = parsedline[5].split("[EC:")
+               KOfeatures[KONumber]["KO_definition"] = rest[0][1:]
+               if len(rest)>1:
+                    KOfeatures[KONumber]["KO_ECNumber"] = rest[1][:-2]
+               KOfeatures[KONumber]["KO_pathway"] = "%s, %s, %s" % (pathA, pathB, pathC)
+
+for feat in KOfeatures:
+     #print KOfeatures[feat]["KO_name"]
+     #print "%s: %s, %s" % (feat, KOfeatures[feat]["KO_name"], KOfeatures[feat]["KO_definition"])
+     print "%s: %s" % (feat, KOfeatures[feat])
+
+ 
+
+
+
+
 KODetailsFile = open(KODetailsFilename, "r")
